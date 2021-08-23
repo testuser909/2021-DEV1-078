ce7475b316b3e50b90f0edcfb29d41acb05da65c
Tawanda Matenhese <tawanda.matenhese@playsafesa.com>
2021-08-23 19:45:38 +0200

    added winning and draw logic

--- src/main/java/com/gametictactoe/tictactoe/GameManager.java
+++ src/main/java/com/gametictactoe/tictactoe/GameManager.java
@@ -2,9 +2,9 @@ package com.gametictactoe.tictactoe;
 
 public class GameManager {
 	private Character[][] gameBoard = { { '\0', '\0', '\0' }, { '\0', '\0', '\0' }, { '\0', '\0', '\0' } };
-
+	private static final int SIZE = 3;
 	private char previousPlayer='\0';
-	public void playGame(int i, int j, char c) {
+	public String playGame(int i, int j, char c) {
 		
         if (i < 1 || j > 3) {
             throw new RuntimeException("Pawn is outside the board");
@@ -13,9 +13,17 @@ public class GameManager {
         if (i > 3 || j < 1) {
             throw new RuntimeException("Pawn is outside the board");
         }
+        
         setGameBox(i,j,previousPlayer,gameBoard);  
         previousPlayer= c;
         
+		if (isAGameWin(i, j, previousPlayer, gameBoard)) {
+			return "Player " + previousPlayer + " Wins";
+		} else if (gameIsADraw(gameBoard)) {
+			return "It is a draw";
+		} else {
+			return "Game In Progress, Next Player Please!!!";
+		}       
 	}
 	
     public void nextPlayer(char lastPlayer, char newPlayer) {
@@ -37,5 +45,33 @@ public class GameManager {
 			gameBoard[x - 1][y - 1] = lastPlayer;
 		}
 	}    
-    
+	public boolean isAGameWin(int x, int y, char lastPlayer, Character[][] gameBoard) {
+		int playerTotal = lastPlayer * SIZE;
+		char horizontalWin, verticalWin, rightToLeftDiagonalWin, leftToRightDiagonalWin;
+
+		horizontalWin = verticalWin = rightToLeftDiagonalWin = leftToRightDiagonalWin = '\0';
+
+		for (int i = 0; i < SIZE; i++) {
+			horizontalWin += gameBoard[i][y - 1];
+			verticalWin += gameBoard[x - 1][i];
+			rightToLeftDiagonalWin += gameBoard[i][i];
+			leftToRightDiagonalWin += gameBoard[i][SIZE - i - 1];
+		}
+		if (horizontalWin == playerTotal || verticalWin == playerTotal || rightToLeftDiagonalWin == playerTotal
+				|| leftToRightDiagonalWin == playerTotal) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean gameIsADraw(Character[][] gameBoard) {
+		for (int x = 0; x < SIZE; x++) {
+			for (int y = 0; y < SIZE; y++) {
+				if (gameBoard[x][y] == '\0') {
+					return false;
+				}
+			}
+		}
+		return true;
+	}    
 }
--- src/test/java/com/gametictactoe/tictactoe/GameManagerTest.java
+++ src/test/java/com/gametictactoe/tictactoe/GameManagerTest.java
@@ -58,5 +58,68 @@ public class GameManagerTest {
 			gameManager.nextPlayer(lastPlayer, newPlayer);
 		});
 	}	
+	
+
+    @Test
+    public void whenPlayThenNoWinner() {
+        String actual = gameManager.playGame(1, 1,'X');
+        Assertions.assertEquals("Game In Progress, Next Player Please!!!", actual);
+    }    
+        
+    @Test
+    public void whenPlayAndWholeHorizontalLineThenWinner() {
+        gameManager.playGame(1,1,'X'); // X
+        gameManager.playGame(1,2,'O'); // O
+        gameManager.playGame(2,1,'X'); // X
+        gameManager.playGame(2, 2,'O'); // O
+        String finalResult = gameManager.playGame(3, 1,'X'); // X
+        Assertions.assertEquals("Player X Wins", finalResult);
+    }
+  
+    @Test
+    public void whenPlayAndWholeVerticalLineThenWinner() {
+        gameManager.playGame(2,1,'X'); // X
+        gameManager.playGame(1,1,'O'); // O
+        gameManager.playGame(3,1,'X'); // X
+        gameManager.playGame(1,2,'O'); // O
+        gameManager.playGame(2,2,'X'); // X
+        String actual = gameManager.playGame(1, 3,'O'); // O
+        Assertions.assertEquals("Player O Wins", actual);
+    }
+    
+    @Test
+    public void whenPlayAndTopBottomDiagonalLineThenWinner() {
+        gameManager.playGame(1,1,'X'); // X
+        gameManager.playGame(1,2,'O'); // O
+        gameManager.playGame(2,2,'X'); // X
+        gameManager.playGame(1,3,'O'); // O
+        String actual = gameManager.playGame(3,3,'X'); // X
+        Assertions.assertEquals("Player X Wins", actual);
+    }
+    
+    @Test
+    public void whenPlayAndBottomTopDiagonalLineThenWinner() {
+        gameManager.playGame(1,3,'X'); // X
+        gameManager.playGame(1,1,'O'); // O
+        gameManager.playGame(2,2,'X'); // X
+        gameManager.playGame(1,2,'O'); // O
+        String actual = gameManager.playGame(3,1,'X'); // X
+        Assertions.assertEquals("Player X Wins", actual);
+    }
+    
+    @Test
+    public void whenAllBoxesAreFilledThenDraw() {
+        gameManager.playGame(1,1,'X');
+        gameManager.playGame(1,2,'O');
+        gameManager.playGame(1,3,'X');
+        gameManager.playGame(2,1,'O');
+        gameManager.playGame(2,3,'X');
+        gameManager.playGame(2,2,'O');
+        gameManager.playGame(3,1,'X');
+        gameManager.playGame(3,3,'O');
+        String actual = gameManager.playGame(3,2,'X');
+        Assertions.assertEquals("It is a draw", actual);
+    }   	
+	
 }
 
